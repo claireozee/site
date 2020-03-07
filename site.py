4dfc0cafb53acb9c967b74962944d18e6cf91660
ozeecodes <47978741+ozeecodes@users.noreply.github.com>
2020-03-07 15:33:28 +0000

    update

--- site.py
+++ site.py
@@ -1,1 +1,22 @@
+from flask import Flask, render_template, request
+import requests
 
+app = Flask("PortfolioSite")
+
+@app.route("/")
+def home():
+        return render_template("home.html")
+
+@app.route("/experience")
+def experience():
+        return render_template("experience.html")
+
+@app.route("/projects")
+def projects():
+        return render_template("projects.html")
+
+@app.route("/contact")
+def contact():
+        return render_template("contact.html")
+
+app.run(port=5001)
