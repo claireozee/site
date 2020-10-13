d4b86d2663e532f2a7fb33a75b639117c7ffb348
ozeecodes <47978741+ozeecodes@users.noreply.github.com>
2020-10-13 12:49:59 +0100

    renamed site.py to app.py to remove flask error, and did pip3 install flask in venv

---
+++ app.py
@@ -0,0 +1,37 @@
+import os
+from flask import Flask, render_template, request, send_from_directory
+import requests
+
+#app = Flask("PortfolioSite")
+#app = Flask("__name__")
+app = Flask("__name__")
+
+@app.route("/")
+#@app.route("/", methods=['GET'])
+def home():
+        return render_template("home.html")
+
+@app.route("/creative")
+def creative():
+        return render_template("creative.html")
+
+@app.route("/experience")
+def experience():
+        return render_template("experience.html")
+
+@app.route("/projects")
+def projects():
+        return render_template("projects.html")
+
+@app.route("/blog")
+def blog():
+        return render_template("blog.html")
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               #'favicon.ico', mimetype='image/vnd.microsoft.icon')
+                               'favicon.ico', mimetype='image/png')
+ 
+#app.run(port=5002) setting port will cause Heroku H10 error - site crash
+app.run(debug=True)
